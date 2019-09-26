22453dd2eb423c7c3da75ad0ce7a0c4880ab11a9
Brian Flad <bflad417@gmail.com>
2019-09-26 09:11:20 -0400

    deps: Migrate from github.com/hashicorp/terraform to github.com/hashicorp/terraform-plugin-sdk
    Reference: https://www.terraform.io/docs/extend/plugin-sdk.html
    
    Updated via:
    
    ```console
    $ tf-sdk-migrator migrate
    $ go get github.com/terraform-providers/terraform-provider-tls@v2.1.1+incompatible
    $ go mod tidy
    $ go mod vendor
    ```
    
    Output from acceptance testing (provided as a smoke test):
    
    ```
    --- PASS: TestAccAWSProvider_Region_AwsCommercial (2.85s)
    --- PASS: TestAccAWSProvider_Region_AwsGovCloudUs (2.85s)
    --- PASS: TestAccAWSProvider_Region_AwsChina (2.86s)
    --- PASS: TestAccAWSProvider_Endpoints_Deprecated (3.06s)
    --- PASS: TestAccAWSProvider_Endpoints (3.11s)
    ```

--- aws/resource_aws_vpc_peering_connection_options_test.go
+++ aws/resource_aws_vpc_peering_connection_options_test.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 )
 
 func TestAccAWSVpcPeeringConnectionOptions_basic(t *testing.T) {
